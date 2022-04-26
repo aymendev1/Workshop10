b5bf0f2f72cb23b917ff6be6b487d6a22329004c
Ayman Azougar <topayman5@gmail.com>
2022-04-26 14:25:26 +0200

    Initial commit

---
+++ src/Task01.java
@@ -0,0 +1,40 @@
+import com.sun.source.doctree.SystemPropertyTree;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Task01 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis;
+        int bajt;
+        try{
+            fis=new FileInputStream("test123.txt");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return;
+        }
+
+        //READ
+        try{
+            do{
+                bajt=fis.read();//read from user
+                if (bajt!= -1){System.out.print((char)bajt);}
+            }
+            while(bajt!= -1);
+        } catch (IOException e) {
+            System.out.println("Error Reading The File");
+        }
+        finally {
+            try{
+            fis.close();
+            System.out.println("File Closed");
+            }
+            catch (IOException e) {
+                System.out.println("Error Closing The File");
+            }
+        }
+
+    }
+}
